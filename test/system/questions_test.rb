3080ca289c850af14a4a76f958d3201c7ce7c8e8
Christoph Niehaus <cniehaus@slb.com>
2022-05-16 19:50:19 +0200

    rails stupid coaching

---
+++ test/system/questions_test.rb
@@ -0,0 +1,26 @@
+# test/system/questions_test.rb
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  # [...]
+
+  test 'saying Hello yields a grumpy response from the coach' do
+    visit ask_url
+    fill_in 'question', with: 'Hello'
+    click_on 'Ask!'
+
+    assert_text 'I don\'t care, get dressed and go to work!'
+
+    visit ask_url
+    fill_in 'question', with: 'Can I go shopping ?'
+    click_on 'Ask!'
+
+    assert_text 'Silly question, get dressed and go to work!'
+
+    visit ask_url
+    fill_in 'question', with: 'I am going to work'
+    click_on 'Ask!'
+
+    assert_text 'Great!'
+  end
+end
